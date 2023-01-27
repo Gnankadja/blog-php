0d4c031582b00ab4417d041ba75458701fa41d15
Nathan Gnankadja <gnankadjanathan@gmail.Com>
2023-01-27 19:11:50 +0100

    Start Header integration

--- config/global.php
+++ config/global.php
@@ -4,3 +4,6 @@ define('DB_NAME', 'php-blog');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
+
+
+define('LOGO_URL', '/../assets/images/logo-hot-coffee.png');
---
+++ includes/header.php
@@ -0,0 +1,38 @@
+<?php
+require_once(__DIR__ . '/../config/global.php');
+?>
+<!DOCTYPE html>
+<html lang="fr" translate="no">
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Hot Coffee</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+
+</head>
+
+<body>
+
+    <header>
+        <!-- Logo -->
+        <img src=<?= LOGO_URL ?> alt="">
+
+        <!-- Search bar -->
+        <div></div>
+
+        <!-- Navbar -->
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Articles</a></li>
+        </ul>
+
+
+        <!-- Network icons -->
+        <ul>
+            <li><img src="" alt=""></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </header>
