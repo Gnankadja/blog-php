ca1cd68f74d7640957745e11207278b56d553b42
Nathan Gnankadja <gnankadjanathan@gmail.Com>
2023-02-01 02:41:43 +0100

    Start featured post

--- index.php
+++ index.php
@@ -4,4 +4,7 @@ require_once(__DIR__ . "/includes/header.php");
 
 // Include Hero Section
 require_once(__DIR__ . "/includes/hero.php");
+
+//  Display Featured Post 
+require_once(__DIR__ . "/src/posts/view/featuredPost.php");
 ?>
---
+++ src/posts/view/featuredPost.php
@@ -0,0 +1,24 @@
+<!-- Featured Post Bloc -->
+<article>
+    <div>
+
+    <!-- Post Detail -->
+        <div>
+            <h2>long established</h2>
+            <h6>It is a long established fact that a reader
+                will be distracted by the readable content of
+                a page when looking at its layout. The point
+                of using Lorem Ipsum is that....
+            </h6>
+            <div>
+                <div>May 20th 2020</div>
+                <div>Read more</div>
+            </div>
+        </div>
+
+        <!-- Post Image -->
+        <div>
+            <img src="/../src/posts/media/image_1.png" alt="">
+        </div>
+    </div>
+</article>
