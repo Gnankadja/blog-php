6480e6d83e818bcd39b293f484b550ae31c1bf1b
Nathan Gnankadja <gnankadjanathan@gmail.Com>
2023-02-01 02:27:42 +0100

    Finalize hero section

---
+++ includes/hero.php
@@ -0,0 +1,12 @@
+<section class="hero-section">
+    <!-- Hero title and subtile bloc -->
+    <div>
+        <h2>Make better <br> coffee <img src="../assets/images/coffee-icon.svg" alt=""></h2>
+        <h4>why learn how to blog?</h4>
+    </div>
+
+    <!-- Hero image section -->
+    <div>
+        <img src="../assets/images/hero-img.png" alt="">
+    </div>
+</section>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
+// Include Header
 require_once(__DIR__ . "/includes/header.php");
+
+// Include Hero Section
+require_once(__DIR__ . "/includes/hero.php");
 ?>
