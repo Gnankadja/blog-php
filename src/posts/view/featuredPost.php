83b068e0b72255eeb9171acd9e42f5bedd79c528
Nathan Gnankadja <gnankadjanathan@gmail.Com>
2023-02-07 00:40:34 +0100

    Finalize Featured Post

--- src/posts/view/featuredPost.php
+++ src/posts/view/featuredPost.php
@@ -1,8 +1,7 @@
 <!-- Featured Post Bloc -->
-<article>
-    <div>
-
-    <!-- Post Detail -->
+<article class="posts-container">
+    <div class="featured-post-wrapper">
+        <!-- Post Detail -->
         <div>
             <h2>long established</h2>
             <h6>It is a long established fact that a reader
@@ -11,8 +10,8 @@
                 of using Lorem Ipsum is that....
             </h6>
             <div>
-                <div>May 20th 2020</div>
-                <div>Read more</div>
+                <div class="post-meta-date">May 20th 2020</div>
+                <a class="post-btn-read-more-link" href="#">Read more</a>
             </div>
         </div>
 
