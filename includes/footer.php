4e77cb7012c7ffc0970372f29699d57a5fe83b0e
Nathan Gnankadja <gnankadjanathan@gmail.Com>
2023-02-08 12:42:31 +0100

    Finalize footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,13 @@
 </body>
 
+<footer>
+    <div>
+        <h5>hotcoffee</h5>
+        <h6>2020 copyright all rights reserved</h6>
+    </div>
+
+    <!-- Social Network icons -->
+    <?php require(__DIR__ . "/socialNetwork.php"); ?>
+</footer>
+
 </html>
