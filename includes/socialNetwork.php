b58078e6490c4718e8ad8c7bc35a907219141a6e
Nathan Gnankadja <gnankadjanathan@gmail.Com>
2023-02-08 12:42:06 +0100

    Create an include file for social icon

---
+++ includes/socialNetwork.php
@@ -0,0 +1,6 @@
+<!-- Social Network icons -->
+<ul>
+    <li class="social-icon"><a href="#"><img src="/../assets/images/instagram-icon.svg" alt=""></a></li>
+    <li class="social-icon"><a href="#"><img src="/../assets/images/twitter-icon.svg" alt=""></a></li>
+    <li class="social-icon"><a href="#"><img src="/../assets/images/linkedin-icon.svg" alt=""></a></li>
+</ul>
